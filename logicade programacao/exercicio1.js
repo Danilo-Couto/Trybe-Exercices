7d33ffd27f9c0f8a3f2b9329472eebcd0d5a3f71
Danilo-Couto <couto.danilo95@gmail.com>
2021-08-27 13:35:38 -0300

    subindo todos os exercicios 4.2 - e preciso separa-lo em arquivos diferentes

---
+++ logicade programacao/exercicio1.js
@@ -0,0 +1,42 @@
+/* exercicio 1
+
+f = 200;
+var celsius = (f-32)/1.8; 
+console.log (celsius) */
+
+
+/* exercicio 2
+
+c = 100; 
+var farenheit = (c*1.8)+32; 
+console.log (farenheit)
+ */
+
+/* exercicio 3
+
+var nomecompleto = "Wesley Safadão"
+var idade = 30
+var apresentacao = "Meu nome é "+nomecompleto+" e tenho "+idade+" anos de vida."
+console.log (apresentacao)
+ */
+
+
+var marina = 20;
+var sivia = 16;
+var iza = 41;
+
+function maisnova (marina, silvia, iza) {
+
+
+        if (marina < silvia & marina < iza) {
+        console.log ("Marina é a mais nova");
+    }
+    else if (sivia < marina & silvia < iza){
+        console.log ("Silvia é a mais nova");
+    }
+    else {
+        console.log ("Iza é a mais nova");
+    }
+}
+
+
