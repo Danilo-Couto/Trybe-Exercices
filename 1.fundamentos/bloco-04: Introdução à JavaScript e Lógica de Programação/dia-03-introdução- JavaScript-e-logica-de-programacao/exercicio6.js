c8c8230a9f444c7d9ce9d1e7b1124f29ae068121
Danilo-Couto <couto.danilo95@gmail.com>
2021-08-30 09:10:18 -0300

    subindo exercicio 6

--- 1.fundamentos/bloco-04: Introdução à JavaScript e Lógica de Programação/dia-03-introdução- JavaScript-e-logica-de-programacao/exercicio6.js
+++ 1.fundamentos/bloco-04: Introdução à JavaScript e Lógica de Programação/dia-03-introdução- JavaScript-e-logica-de-programacao/exercicio6.js
@@ -1,7 +1,8 @@
+/* 
 6- Faça um programa que diz se um número definido numa variável é primo ou não.
 Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
 
-/* 
+
 let n = 521;
 let ehprimo = "";
 
