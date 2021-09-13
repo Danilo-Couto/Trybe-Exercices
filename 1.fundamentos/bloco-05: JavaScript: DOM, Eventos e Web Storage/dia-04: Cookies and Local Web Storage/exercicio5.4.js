6a251c3b09bdd59b78d450085fdf49d34348413f
Danilo-Couto <couto.danilo95@gmail.com>
2021-09-13 13:14:13 -0300

    exericio 5.4 completo

--- 1.fundamentos/bloco-05: JavaScript: DOM, Eventos e Web Storage/dia-04: Cookies and Local Web Storage/exercicio5.4.js
+++ 1.fundamentos/bloco-05: JavaScript: DOM, Eventos e Web Storage/dia-04: Cookies and Local Web Storage/exercicio5.4.js
@@ -1,4 +1,5 @@
-/* As pessoas devem ter o poder de alterar:
+/* 
+As pessoas devem ter o poder de alterar:
 Cor de fundo da tela;
 Cor do texto;
 Tamanho da fonte;
@@ -9,21 +10,22 @@ Tipo da fonte ( Font family ).
 window.onload = function () {
     let select = document.querySelectorAll('select')[0];
     select.addEventListener('change', function(){
-        let colorSelected = select.selectedOptions[0];
-        document.body.style.backgroundColor = colorSelected.value;
+        let colorSelected = select.selectedOptions[0].value;
 
-        localStorage.setItem('bg',colorSelected.value);
+        document.body.style.backgroundColor = colorSelected;
+
+        localStorage.setItem('bg',colorSelected);
     })
-
+      
     let fontColorSelected = document.querySelectorAll('select')[1];
     fontColorSelected.addEventListener('change', function(){
-        let fontColor = fontColorSelected.selectedOptions[0];
+        let fontColor = fontColorSelected.selectedOptions[0].value;
         let p =document.querySelector('p');
-        p.style.color = fontColor.value;
+        p.style.color = fontColor;
 
-        localStorage.setItem ('cordafonte',fontColor.value);
-    })
-   
+        localStorage.setItem ('cordafonte',fontColor);
+    });
+    
     let inputFontSize = document.querySelector('input[name="fontSize"]');
     inputFontSize.addEventListener ('change', function(){
         let p =document.querySelector('p');
@@ -31,6 +33,26 @@ window.onload = function () {
 
         localStorage.setItem ('fontSize', `${inputFontSize.value}px`);
     })
+    
+     let textSpace = document.getElementsByTagName('input')[1]; 
+    textSpace.addEventListener ('change', spacement);
+    function spacement() {
+        let p = document.querySelector('p');
+        p.style.lineHeight = textSpace.value;
+        //p.style.lineHeight = `${textSpace.value}px`;
+
+        localStorage.setItem ('textSpace',textSpace.value);
+        };   
+
+    let fontSizeFamily = document.querySelectorAll('select')[2];    
+    fontSizeFamily.addEventListener('change', fontFamily);
+    function fontFamily () {
+        let p = document.querySelector('p');
+        let fontFamilySelected = fontSizeFamily.selectedOptions[0].value;
+        p.style.fontFamily = fontFamilySelected;
+
+        localStorage.setItem('fontFamily', fontFamilySelected);
+    }
 
     let savedBG =localStorage.getItem('bg');
     document.body.style.backgroundColor = savedBG;
@@ -41,4 +63,10 @@ window.onload = function () {
 
     let savedFont = localStorage.getItem('fontSize');
     p.style.fontSize = savedFont;
+
+    let savedTextSpace = localStorage.getItem('textSpace');
+    p.style.lineHeight = savedTextSpace;
+
+    let savedFontFamily = localStorage.getItem ('fontFamily');
+    p.style.fontFamily = savedFontFamily;
 }; 
