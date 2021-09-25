20b622f272bcd66f22445a5679e322c7ff0ed65b
Danilo-Couto <couto.danilo95@gmail.com>
2021-09-25 08:58:25 -0300

    finalizado parte 1

---
+++ 1.fundamentos/bloco-07: Introdução à JavaScript ES6 e Testes Unitários/dia1 : JavaScript ES6 - let, const, arrow functions e template literals/script.js
@@ -0,0 +1,32 @@
+/*
+1.
+Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+Modifique a estrutura da função para que ela seja uma arrow function .
+Modifique as concatenações para template literals .
+ */
+
+/* //FUNCAO ORIGINAL
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+testingScope(true); */
+
+//FUNCAO OTIMIZADA
+testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+testingScope(true);
