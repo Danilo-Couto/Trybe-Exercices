2adda3a43f342a94ab0cefc2ddffb7c4d101202a
Danilo Luzzi do Couto <couto.danilo95@gmail.com>
2021-09-27 16:30:34 -0300

    exercicios dia 2 bloco 7

--- 1.fundamentos/bloco-07: Introdução à JavaScript ES6 e Testes Unitários/dia2:JavasSript ES6 - Objects/pre-pt1b.js
+++ 1.fundamentos/bloco-07: Introdução à JavaScript ES6 e Testes Unitários/dia2:JavasSript ES6 - Objects/pre-pt1b.js
@@ -1,6 +1,6 @@
 /* crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele. */
 
-objectCreator = (a, b, c ) => {
+/* objectCreator = (a, b, c ) => {
   a = {  };
    
   let newKey = 'brand';
@@ -12,7 +12,19 @@ objectCreator = (a, b, c ) => {
 
   console.log(a); 
  }
+ */
 
- let object = {
+ function objectCreator(a, b, c ) {
+    a = {  };
+    
+    let newKey = 'brand';
+    const brand = b;
+    a[newKey] = brand;
+    let newKey2 = 'model';
+    const model = c;
+    a[newKey2] = model;
 
+  return a; 
  }
+
+console.log (objectCreator ('motorcycle','Yamaha', 'Tenere 250'));
