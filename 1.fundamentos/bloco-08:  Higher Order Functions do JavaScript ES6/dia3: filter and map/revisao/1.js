b0a8779db713b97c4d6e035d6ab6aff0f487d470
Danilo Couto <couto.danilo95@gmail.com>
2021-10-10 16:54:06 -0300

    revisao reduce

--- 1.fundamentos/bloco-08:  Higher Order Functions do JavaScript ES6/dia3: filter and map/revisao/1.js
+++ 1.fundamentos/bloco-08:  Higher Order Functions do JavaScript ES6/dia3: filter and map/revisao/1.js
@@ -61,19 +61,19 @@ const books = [
     },
   ];
   
-  //3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
-  const expectedResult = {
-    author: {
-      birthYear: 1948,
-      name: 'George R. R. Martin',
-    },
-    genre: 'Fantasia',
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    releaseYear: 1991,
-  };
+  //1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+  //Dica: Use a função map
+ 
+  const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
   
-  function getNamedBook() {
-    return books.find((book)=>book.name.length===26).name  
-  }
-  console.log(getNamedBook()  )
+function formatedBookNames() {
+    return books.map((book)=> `${book.name} - ${book.genre} - ${book.author.name}`)
+}
+console.log(formatedBookNames())
---
+++ 1.fundamentos/bloco-08:  Higher Order Functions do JavaScript ES6/dia3: filter and map/revisao/draft.js
@@ -0,0 +1,14 @@
+const lista = [
+    {id: 1, nome: "gol"},
+    {id: 2, nome: "ferrari"},
+    {id: 3, nome: "camaro :)"}
+];
+
+const callback = (acumulador, valor) => {
+    acumulador[valor.id] = valor.nome
+    console.log(acumulador)
+    return acumulador
+};
+//const valorInicial = {};
+const carrosIndexadosPorId = lista.reduce(callback, {});
+console.log(carrosIndexadosPorId[3]) //camaro :)
