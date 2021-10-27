78780c2338fca41b1297981752e39af57970de06
Danilo Couto <couto.danilo95@gmail.com>
2021-10-27 15:35:58 -0300

    exercicio/10.3

---
+++ 2.frontEnd/bloco10/dia1.introReact/exercices/10.1.3/src/Header.jsx
@@ -0,0 +1,15 @@
+// No arquivo Header.jsx , crie uma classe React, chamada Header . Essa classe deve renderizar uma tag h1 com o texto 'Conteúdos de Front-End'. Não esqueça de exportar esse componente;
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return ( 
+            <div>
+                <h1 className='h1'> Conteúdos de Front-End</h1>  
+            </div>
+        )
+    }
+}
+
+export default Header;
