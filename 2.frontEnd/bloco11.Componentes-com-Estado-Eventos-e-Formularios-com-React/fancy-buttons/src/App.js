1128fd0d5053fe8246ebdc1d61a74ccd7e626076
Danilo Couto <couto.danilo95@gmail.com>
2021-11-02 20:04:26 -0300

    Unindo componentes com estados e eventos

--- 2.frontEnd/bloco11.Componentes-com-Estado-Eventos-e-Formularios-com-React/fancy-buttons/src/App.js
+++ 2.frontEnd/bloco11.Componentes-com-Estado-Eventos-e-Formularios-com-React/fancy-buttons/src/App.js
@@ -1,45 +1,48 @@
 import './App.css';
 import React, { Component } from 'react';
 
-
 class App extends React.Component {
 
 constructor() {
   super ()
+
   this.handleClick = this.handleClick.bind(this); // this.handleClick é redeclarado dizendo que ele esta ligado a this
   this.handleClick2 = this.handleClick2.bind(this);
   this.handleClick3 = this.handleClick3.bind(this);
-  console.log('componente sendo construído')
+
+  this.state = {
+    numeroDeCLiquesBotao1: 0,
+    numeroDeCLiquesBotao2: 0,
+    numeroDeCLiquesBotao3: 0,
+  }
 }
 
 handleClick() {
-  console.log(this)
-  console.log('clicou')
+  this.setState ((estadoAnterior, _props) => ({
+    numeroDeCLiquesBotao1: estadoAnterior.numeroDeCLiquesBotao1+1
+    }))
 }
 
 handleClick2() {
-  console.log(this)
-  console.log('clicou de novo')
+  this.setState ((estadoAnterior, _props) => ({
+    numeroDeCLiquesBotao2: estadoAnterior.numeroDeCLiquesBotao2+1
+  }))
 }
 
 handleClick3() {
-  console.log(this)
-  console.log('clicou mais uma vez')
+  this.setState ((estadoAnterior, _props) => ({
+    numeroDeCLiquesBotao3: estadoAnterior.numeroDeCLiquesBotao3+1
+  }))
 }
 
   render () { 
-    console.log(this)
     return (
     <div>
-      <button onClick={this.handleClick}> Meu botão </button>
-      <button onClick={this.handleClick2}> Meu   botão 2</button>
-      <button onClick={this.handleClick3}> Meu botão 3 </button> 
+      <button onClick={this.handleClick}> {this.state.numeroDeCLiquesBotao1} </button>
+      <button onClick={this.handleClick2}> {this.state.numeroDeCLiquesBotao2} </button>
+      <button onClick={this.handleClick3}> {this.state.numeroDeCLiquesBotao3} </button> 
      </div>
    );}
 }
 
 export default App;
-
-// 1 - Crie uma aplicação React com npx create-react-app fancy-buttons . Altere o componente App.js para que seja um componente de classe e contenha um botão associado a um evento que imprime um texto qualquer via console.log() . Não precisa se preocupar com a sintaxe correta para funções de eventos por enquanto!!
-
-// 2 - Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log() .
