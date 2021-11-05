eb6454fb7018ce1679c0c839b29517965c7ead21
Danilo Couto <couto.danilo95@gmail.com>
2021-11-05 13:12:42 -0300

    formulários com react

--- 2.frontEnd/bloco11.Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-2-formularios-no-react/componentes-controlados/src/Form.js
+++ 2.frontEnd/bloco11.Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-2-formularios-no-react/componentes-controlados/src/Form.js
@@ -1,5 +1,3 @@
-// 1 - Crie um formulário com um campo select , dois inputs de tipo diferente, uma textarea e faça de um deles um componente controlado, ou seja, elementos do formulário controlados pelo Estado
-
 import React from 'react'
 import './Form.js'
 
@@ -7,27 +5,36 @@ class Form extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleChangeTextArea = this.handleChangeTextArea.bind(this);
-    this.handleChangeInputName = this.handleChangeInputName.bind(this);
-    this.handleChangeSelectFruit = this.handleChangeSelectFruit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  
+
     this.state = {
       dissertacao: 'Por favor, escreva uma dissertação sobre frutas',
       fruta: '',
       nome: 'Digite seu nome de fruta',
+      likeFruit: Boolean,
     };
   }
 
-  handleChangeTextArea(event) {
-    this.setState({dissertacao: event.target.value});
+  handleChange({target}){
+    const {name,value} = target;
+
+    this.setState ({
+      [name]: value,
+    })
   }
-  handleChangeSelectFruit(event) {
-    this.setState ({fruta: event.target.value});
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
-  handleChangeInputName(event) {
-    this.setState({nome: event.target.value});
-  }
+  
   handleSubmit(event) {
     alert(`${this.state.nome}, sua fruta favorita é ${this.state.fruta}`);
     event.preventDefault();
@@ -36,44 +43,76 @@ class Form extends React.Component {
   render() {
     return (
       <div>
-
         <h1> Meu Form </h1>
         <form onSubmit={this.handleSubmit}>
-
-          <label>
+            
+        <fieldset>
+          <label htmlFor="dissertação">
             Dissertação:
             <textarea 
               name='dissertacao'
               value={this.state.dissertacao}
-              onChange={this.handleChangeTextArea} />
+              onChange={this.handleChange} />
           </label>
+        </fieldset>
 
-          Qual sua fruta favorita?
+        <fieldset>
+        Qual sua fruta favorita?
           <select 
             name='fruta'
             value={this.state.fruta} 
-            onChange={this.handleChangeSelectFruit}>
+            onChange={this.handleChange}>
               <option value="laranja">Laranja</option>
               <option value="limao">Limão</option>
               <option value="coco">Coco</option>
               <option value="manga">Manga</option>
-            </select> 
+          </select> 
+        </fieldset>
 
-          <label>
-            Nome:
+
+ {/*  <input type="checkbox" id="html" name="fav_language" value="HTML">
+ </input>
+  <label for="html">HTML</label>  */}
+
+        <fieldset>
+            Você gosta de fruta?
+            <input 
+              name='likeFruit'
+              type='checkbox'
+              id = 'likeFruit'
+              checked={this.state.likeFruit}
+              onChange={this.handleInputChange}      
+              /> 
+            <label htmlFor="likeFruit"> Sim </label>
+
+{/* COMO FAZER TYPO RADIO */}
+{/*             <input 
+              name='likeFruit'
+              type='checkbox'
+              id = 'likeFruit'
+              checked={this.state.likeFruit}
+              onChange={this.handleInputChange}      
+              /> 
+            <label htmlFor="notLikeFruit"> Não </label> */}
+        
+        </fieldset>            
+        <fieldset>
+          <label htmlFor="nome">
+          Nome:
             <input 
               type="text" 
               name='nome'
               value={this.state.nome}
-              onChange={this.handleChangeInputName} />
+              onChange={this.handleChange} />
           </label>
-          <input 
+        </fieldset>
+  
+        <input 
             type="submit"
-            value="Enviar" />
-          
-        </form>
-
-      </div>
+            value="Enviar" />   
+        <input type="file" />
+      </form>
+    </div>
     );
   }
 }
