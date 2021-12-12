2183cd05c75c85e52b1067559a2e8df588ac1a1e
Danilo Couto <couto.danilo95@gmail.com>
2021-12-12 15:16:08 -0300

    ultimo exercicio blobo 8.4

--- revisao de ferias/Hofs/8.4/6.js
+++ revisao de ferias/Hofs/8.4/6.js
@@ -2,11 +2,28 @@
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-function studentAverage() {
+function averageGrades(i) {
+    const sum = grades[i].reduce((sum, grade) => {
+      return (sum+grade)
+  },0);
+  return sum/grades[i].length
 }
 
-const expected = [
-  { name: 'Pedro Henrique', average: 7.8 },
-  { name: 'Miguel', average: 9.2 },
-  { name: 'Maria Clara', average: 8.8 },
-];
+function studentAverage() {
+  return students.map((student, i)=> {
+    return { name: student, average: averageGrades(i)}
+  })
+}
+console.log(studentAverage())
+
+/* GABARITO:
+function studentAverage() {
+  const nameAndAverage = students.map((student, index) => ({
+    name: student,
+    average: (notes[index].reduce((acc, curr) => acc + curr, 0) / notes[index].length),
+  }));
+  return nameAndAverage;
+}
+ */
+
+
