6396b6ec374d73f809ce3be4dd5dd237c974c8b8
Danilo Couto <couto.danilo95@gmail.com>
2021-12-17 19:08:10 -0300

    fixacao

---
+++ 2.frontEnd/bloco15.Redux/bloco15.dia1-GerenciamentoDeEstadoComRedux/pre-exercices/combineReducers.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+// Importando o reducer combinado que fizemos logo acima
+import reducerCombinado from './reducers/index';
+
+const store = createStore(reducerCombinado);
+
+console.log(store.getState())
+
+//{
+// meuReducer: {/_estado do meuReducer_/},
+// meuOutroReducer: {/_estado do meuOutroReducer_/,}
+//}
+...
