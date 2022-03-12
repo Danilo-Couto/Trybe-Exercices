b445dcc140dcbae5d534c2a7a55f66f52cae084f
Danilo Couto <couto.danilo95@gmail.com>
2022-03-12 18:31:46 -0300

    add index.js

--- 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-1-nodejs-um-motor-javascript/my-script/index.js
+++ 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-1-nodejs-um-motor-javascript/my-script/index.js
@@ -1,1 +1,28 @@
-const imc = require('./imc');
+const readline = require('readline-sync');
+
+// OBJ COM OS SCRIPTS
+const scripts = [
+  { name: 'Calcular IMC ', script: './imc.js' },
+  { name: 'Calcular velocidade média ', script: './velocidade.js' },
+  { name: 'Jogo de adivinhação ', script: './sorteio.js' },
+];
+
+// MENSAGEM 
+let mensagem = scripts
+  .map((script, index) => `${index + 1} - ${script.name}`);
+
+mensagem.unshift('Escolha um número para executar o script correspondente');
+mensagem = mensagem.join('\n');
+
+// A ESCOLHA DA OPÇÃO
+const scriptNumber = readline.questionInt(mensagem) - 1;
+
+// ENTRANDO NO OBJETO ESCOLHIDO
+const script = scripts[scriptNumber];
+if (!script) console.log('Número inválido. Saindo');
+
+// E EXECUTANDO O SCRIPT ESCOLHIDO
+require(script.script);
+
+
+//RESOLUCAO DO GABARITO
