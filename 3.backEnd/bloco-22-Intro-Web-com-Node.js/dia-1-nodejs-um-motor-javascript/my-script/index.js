00ef63fcc4c0f03163e781e7d7567e38584edec0
Danilo Couto <couto.danilo95@gmail.com>
2022-03-12 23:12:46 -0300

    update index.js

--- 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-1-nodejs-um-motor-javascript/my-script/index.js
+++ 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-1-nodejs-um-motor-javascript/my-script/index.js
@@ -5,6 +5,8 @@ const scripts = [
   { name: 'Calcular IMC ', script: './imc.js' },
   { name: 'Calcular velocidade média ', script: './velocidade.js' },
   { name: 'Jogo de adivinhação ', script: './sorteio.js' },
+  { name: 'Fatore x', script: './factorial.js'},
+  { name: 'Sequencia de Fibonacci', script: './fibonacci.js'}
 ];
 
 // MENSAGEM 
