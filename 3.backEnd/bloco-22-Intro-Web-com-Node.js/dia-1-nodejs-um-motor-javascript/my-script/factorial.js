aa15a82a0ba5b0421c30cd51ea44ceae90ec3c76
Danilo Couto <couto.danilo95@gmail.com>
2022-03-13 17:42:19 -0300

    update factorial, other way

--- 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-1-nodejs-um-motor-javascript/my-script/factorial.js
+++ 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-1-nodejs-um-motor-javascript/my-script/factorial.js
@@ -1,19 +1,29 @@
 const readline = require('readline-sync');
 
-function fatoracao(x) {
+/* function fatoracao(x) {
     if (x === 0 || x === 1)
       return;
     for (let i = x - 1; i >= 1; i--) {
       x *= i;
     }
     return x;
-  }
+  } */
+
+function fatoracao(number) {
+  let result = 1;
+  for (let i = 2; i <= number; i += 1) {
+    result *= i;
+  } 
+  return result;
+}
 
 // do gabarito é mais elegante:
 /* function realizaFatoracao(x) {
     return [0, 1].includes(x)
       ? 1
       : x * realizaFatoracao(x - 1);  */
+
+      
 
 function calculo() {
   const x = readline.questionInt('Informe o valor de x: ');
