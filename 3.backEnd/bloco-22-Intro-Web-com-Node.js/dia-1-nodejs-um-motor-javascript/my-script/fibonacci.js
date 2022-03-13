c08ce4ddaf7f46c24decd9910099cdfe9aac8507
Danilo Couto <couto.danilo95@gmail.com>
2022-03-12 23:12:13 -0300

    add fibonacci.js

---
+++ 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-1-nodejs-um-motor-javascript/my-script/fibonacci.js
@@ -0,0 +1,48 @@
+const readline = require('readline-sync');
+
+// A sequência de fibonacci começa com 0 e 1 e os números seguintes são sempre a soma dos dois números anteriores.
+
+/* function calcFibo(n){   
+    const array = [0,1];
+
+    for (let j = 1; j < n ; j++) {
+        let soma = 0;
+
+        for (let i = 0; i < array.length; i++) {
+            soma += array[i];
+        }
+
+        array.push(soma)
+    }
+    return array.splice(1);
+} */
+
+
+function calcFibo(n){   
+    const array = [0,1];
+    for (let j = 1; j < n ; j++) {
+        let soma = 0;
+
+        array.forEach(el => {
+            soma+=el
+        });
+
+        array.push(soma)
+    }
+    return array.splice(1);
+}
+
+function calculo() {
+    const n = readline.questionInt('Informe o valor de n para a Sequencia de Fibonacci: ');
+    if (n <= 0) {
+        console.log('Digite um número maior que 0!')
+        return;
+    }
+  
+    const resultado = calcFibo(n);
+    
+    console.log(`n: ${n}`);
+    console.log(`Resultado: ${resultado}`);
+  }
+  
+calculo();
