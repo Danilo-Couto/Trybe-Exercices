a9a2a1683f1604becb4892fdd9a5beed92d18ac8
Danilo Couto <couto.danilo95@gmail.com>
2022-03-18 18:43:49 -0300

    bloco22.5: atividade3

---
+++ 3.backEnd/bloco-22-Intro-Web-com-Node.js/dia-5-express-middlewares/exercicios/atividade-3/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const postRouter = require('./postRouter');
+const errors = require('./routerNotFound');
+const PORT = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.get('./posts', userRouter);
+
+app.use('*', (_req, _res, next) => next({ statusCode: 404, message: 'Opsss router not found' }));
+app.use(errors.routerNotFound);
+
+app.listen(PORT, () => {
+  console.log('Aplicação ouvindo na porta 3000');
+});
+
