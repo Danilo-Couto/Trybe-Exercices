a8e63618035eb9cd441bd7f2bf3e8c118fa4ec9a
Danilo Couto <couto.danilo95@gmail.com>
2022-03-26 17:36:05 -0300

    bloco 23.2: add dinamic validators to controllers at Authors

--- 3.backEnd/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/services/Authors.js
+++ 3.backEnd/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/services/Authors.js
@@ -14,13 +14,6 @@ const fullNameMaker = (authorData) => {
   };
 };
 
-const isValid = (firstName, middleName, lastName) => {
-  if (!firstName || typeof firstName !== 'string') return false;
-  if (!lastName || typeof lastName !== 'string') return false;
-  if (!middleName || typeof middleName !== 'string') return false;
-  return true;
-}
-
 const getAll = async () => {
   const authors = await Author.getAll();
   return authors.map(fullNameMaker);
@@ -32,9 +25,17 @@ const findById = async (id) => {
   return fullNameMaker(author);
 };
 
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return {code: 422, message: 'É obrigatório informar firstName e ele deve ser string'};
+  if (!lastName || typeof lastName !== 'string') return {code: 422, message: 'É obrigatório informar middleName e ele deve ser string'};
+  if (!middleName || typeof middleName !== 'string') return {code: 422, message: 'É obrigatório informar lastName e ele deve ser string'};
+  return {};
+}
+
 const createAuthor = async (firstName, middleName, lastName) => {
-  const validAuthor = isValid (firstName, middleName, lastName);
-  if (!validAuthor) return false;
+  const validAuthor = isValid(firstName, middleName, lastName);
+  if (validAuthor.message) return validAuthor;
+
   const author = await Author.createAuthor(firstName, middleName, lastName);
   return fullNameMaker(author);
 };
